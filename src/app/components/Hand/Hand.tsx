f12cbb6bdb668f943efc9a61989f95d86436eeea
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-05-21 23:53:04 -0500

    Created a hand ring to help with a css rotation problem

--- src/app/components/Hand/Hand.tsx
+++ src/app/components/Hand/Hand.tsx
@@ -6,7 +6,9 @@ const Hand = (props) =>{
 React.useEffect(()=>{console.log(`${props.handStyle} Effect`)});
 
 return(
-  <hand id={props.handStyle}></hand>
+  <handRing>
+    <hand id={props.handStyle}></hand>
+  </handRing>
 )
 }
 
