ff07ddc693d242f3a2866932d3113d20bf1d9e76
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-05-22 11:30:11 -0500

    created global files for custom elemnt declarations

---
+++ src/app/global.d.ts
@@ -0,0 +1,7 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+        'handring': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'hand': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'clockface': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+}
