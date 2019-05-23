e3e547182dcdcf96080920148c7743e00577ed0d
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-05-22 19:54:29 -0500

    gotr numerals on clock

--- src/app/components/Clock/Clock.tsx
+++ src/app/components/Clock/Clock.tsx
@@ -4,13 +4,38 @@ import './Clock.scss'
 import Hand from '../Hand/Hand';
 
 
-const Clock = () =>
+
+const Numeral = ({number}) => {
+  const numeralAngleStyle = {
+    transform: `rotate(${number*30}deg)`
+  };
+
+  const innerNumeralAngleStyle = {
+    transform: `rotate(-${number*30}deg)`
+  };
+
+
+  return(
+    <clocknumber style={numeralAngleStyle} class='numeral'>
+      <subnum style={innerNumeralAngleStyle}>{number}</subnum>
+    </clocknumber>
+  )
+}
+
+
+
+const Clock = () =>{
+  const [numerals, setNumerals] = React.useState([1,2,3,4,5,6,7,8,9,10,11,12])
+
+
+
+return(
   <clockface>
+    {numerals.map((num) => <Numeral number={num}/>)}
     <Hand handType="Second" />
     <Hand handType="Minute" />
     <Hand handType="Hour" />
   </clockface>
-
-
+)}
 
 export default Clock;
