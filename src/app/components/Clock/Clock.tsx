104e4304379181e19ac347181c7f8a4d98b786fd
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-06-02 22:44:44 -0500

    moved handTypes to array

--- src/app/components/Clock/Clock.tsx
+++ src/app/components/Clock/Clock.tsx
@@ -7,13 +7,12 @@ import {Hand, Numeral} from '../../components'
 
 const Clock = () =>{
 const [numerals, setNumerals] = React.useState([1,2,3,4,5,6,7,8,9,10,11,12])
+const [handTypes, setHandTypes] = React.useState([`Second`, `Minute`, `Hour`])
 
   return(
     <clock-face>
       {numerals.map((num) => <Numeral key={num} number={num}/>)}
-      <Hand handType="Second" />
-      <Hand handType="Minute" />
-      <Hand handType="Hour" />
+      {handTypes.map((hand) => <Hand handType={hand} />)}
     </clock-face>
   )
 }
