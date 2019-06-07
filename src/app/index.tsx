f32c3de18c3c13663c40983c4ab47defc626e1d9
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-06-06 21:10:54 -0500

    removed Hand from import

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {render} from 'react-dom';
 import './index.scss'
 
-import {Hand, Clock} from './components'
+import {Clock} from './components'
 
 const App = () => <Clock />
 
